24fc1fb586e8acec587084e7aa8a7e773841f912
paulinabravo <56414293+paulinabravo@users.noreply.github.com>
2019-10-10 18:03:02 -0400

    commit

--- app/src/main/java/com/example/logcat/MapsActivity.java
+++ app/src/main/java/com/example/logcat/MapsActivity.java
@@ -23,6 +23,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
+    //-------------
 
 
     /**
